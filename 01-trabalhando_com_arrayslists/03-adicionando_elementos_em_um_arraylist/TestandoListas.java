0b0bfbf3d1c12e201cde40b9633e9d8cc96df793
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-02-03 22:10:02 -0300

    Adicionando elementos em um ArrayList

---
+++ 01-trabalhando_com_arrayslists/03-adicionando_elementos_em_um_arraylist/TestandoListas.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TestandoListas {
+
+    public static void main(String[] args) {
+        var cursos = new ArrayList<>();
+
+        cursos.add("Curso 1");
+        cursos.add("Curso 2");
+        cursos.add("Curso 3");
+
+        System.out.println(cursos);
+    }
+}

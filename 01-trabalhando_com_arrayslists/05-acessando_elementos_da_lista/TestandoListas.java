8085fd3f0d9294510e33e803a89d98c0a5f230a9
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-02-03 22:17:23 -0300

    Acessando elementos da lista

---
+++ 01-trabalhando_com_arrayslists/05-acessando_elementos_da_lista/TestandoListas.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class TestandoListas {
+
+    public static void main(String[] args) {
+
+        String curso1 = "Java 8: Tire proveito dos novos recursos da linguagem";
+        String curso2 = "Apache Camel";
+        String curso3 = "Certificacao Java SE 8 Programmer I";
+
+        ArrayList<String> cursos = new ArrayList<>();
+        cursos.add(curso1);
+        cursos.add(curso2);
+        cursos.add(curso3);        
+
+        System.out.println("O primeiro curso da lista eh o: " + cursos.get(0)); // como acessar o primeiro valor?
+    }
+}

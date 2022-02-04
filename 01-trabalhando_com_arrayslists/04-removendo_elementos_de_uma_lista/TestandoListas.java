7fa3d22cfd08c24de77de4dc2eb0e0c421f6c64c
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-02-03 22:12:32 -0300

    Removendo elementos de uma lista

---
+++ 01-trabalhando_com_arrayslists/04-removendo_elementos_de_uma_lista/TestandoListas.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class TestandoListas {
+
+    public static void main(String[] args) {
+
+        String curso1 = "Java 8: Tire proveito dos novos recursos da linguagem";
+        String curso2 = "Apache Camel";
+        String curso3 = "Certificacao Java SE 8 Programmer I";
+
+        ArrayList<String> cursos = new ArrayList<>();
+        cursos.add(curso1);
+        cursos.add(curso2);
+        cursos.add(curso3);        
+
+        // qual metodo chamar aqui?
+        // cursos.remove(curso2);
+        cursos.remove(2);
+
+        System.out.println(cursos);
+    }
+}

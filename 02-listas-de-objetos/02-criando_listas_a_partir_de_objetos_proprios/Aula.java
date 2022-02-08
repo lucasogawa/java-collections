25a7b4aea7f0f4c644bea8de7116f7f4ee64fe4e
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-02-07 22:34:34 -0300

    Criando listas a partir de objetos próprios

---
+++ 02-listas-de-objetos/02-criando_listas_a_partir_de_objetos_proprios/Aula.java
@@ -0,0 +1,20 @@
+
+
+public class Aula {
+    
+    private String titulo;
+    private int tempo;
+
+    public Aula(String titulo, int tempo) {
+        this.titulo = titulo;
+        this.tempo = tempo;
+    }
+
+    public String getTitutlo() {
+        return this.titulo;
+    }
+
+    public int getTempo() {
+        return this.tempo;
+    }    
+}

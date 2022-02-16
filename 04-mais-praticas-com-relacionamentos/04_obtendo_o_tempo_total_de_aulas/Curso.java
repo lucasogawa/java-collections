f146f094976259aed55b84e959000f2be540c591
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-02-15 22:06:36 -0300

    Obtendo o tempo total de aulas

---
+++ 04-mais-praticas-com-relacionamentos/04_obtendo_o_tempo_total_de_aulas/Curso.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Curso {
+
+    private String nome;
+    private String instrutor;
+    private List<Aula> aulas = new LinkedList<Aula>();
+
+    public Curso(String nome, String instrutor) {
+        this.nome = nome;
+        this.instrutor = instrutor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getInstrutor() {
+        return instrutor;
+    }
+
+    public List<Aula> getAulas() {
+        return Collections.unmodifiableList(aulas);
+    }
+
+    public void adiciona(Aula aula) {
+        this.aulas.add(aula);
+    }
+
+    public int getTempoTotal() {
+        return this.aulas
+            .stream()
+            .mapToInt(Aula::getTempo)
+            .sum();
+    }
+}

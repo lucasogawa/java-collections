03f4c2409b61cb7f12d0880ddb413cc9946d2151
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-02-15 21:59:55 -0300

    Ordenando as aulas de um curso

---
+++ 04-mais-praticas-com-relacionamentos/03-ordenando-as-aulas-de-um-curso/TestaCurso.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class TestaCurso {
+
+    public static void main(String[] args) {
+        // crie um novo curso passando o nome e instrutor no construtor
+        Curso curso = new Curso("Java Collections", "Paulo");
+
+        // chame o getter da lista de aulas do curso e imprima o resultado
+        System.out.println(curso.getAulas());
+
+        // adicione novas aulas na classe Curso
+        curso.adiciona(new Aula("Aula 2", 2));
+        curso.adiciona(new Aula("Aula 1", 1));
+        curso.adiciona(new Aula("Aula 3", 3));
+
+        System.out.println(curso.getAulas());
+
+        // ordene as aulas
+        List<Aula> aulasImutaveis = curso.getAulas();
+        List<Aula> aulas = new ArrayList<>(aulasImutaveis);
+
+        Collections.sort(aulas);
+
+        System.out.println(aulas);
+    }
+}

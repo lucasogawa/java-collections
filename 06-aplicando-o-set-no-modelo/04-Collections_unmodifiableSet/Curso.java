5a2c4ac66ba5a596958929861372a10c4212a1fd
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-02-22 22:30:46 -0300

    Adding Collections.unmodifiableSet

---
+++ 06-aplicando-o-set-no-modelo/04-Collections_unmodifiableSet/Curso.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Curso {
+
+    private String nome;
+    private String instrutor;
+    private List<Aula> aulas = new LinkedList<Aula>();
+    private Set<Aluno> alunos = new HashSet<>();
+
+    public Curso(String nome, String instrutor) {
+        this.nome = nome;
+        this.instrutor = instrutor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getInstrutor() {
+        return instrutor;
+    }
+
+    public List<Aula> getAulas() {
+        return Collections.unmodifiableList(aulas);
+    }
+
+    public Set<Aluno> getAlunos() {
+        return Collections.unmodifiableSet(alunos);
+    }
+
+    public void adiciona(Aula aula) {
+        this.aulas.add(aula);
+    }
+
+    public int getTempoTotal() {
+        return this.aulas
+            .stream()
+            .mapToInt(Aula::getTempo)
+            .sum();
+    }
+
+    public void matricula(Aluno aluno){
+        this.alunos.add(aluno);
+    }
+
+    @Override
+    public String toString() {
+        return "[Curso: " + this.nome + ", tempo total: " + getTempoTotal() + ", aulas: [" + aulas + "]";
+    }
+}

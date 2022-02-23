531bd145d082e430f2fba529dfa55622a40b1451
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-02-22 22:25:38 -0300

    Adding Encapsulando o Set

---
+++ 06-aplicando-o-set-no-modelo/03-encapsulando-o-set/TestaCursoComAluno.java
@@ -0,0 +1,23 @@
+public class TestaCursoComAluno {
+
+    public static void main(String[] args) {
+
+        Curso cursos = new Curso("Curso 1", "Instrutor 1");
+
+        cursos.adiciona(new Aula("Aula 1", 1));
+        cursos.adiciona(new Aula("Aula 2", 2));
+        cursos.adiciona(new Aula("Aula 3", 3));
+
+        Aluno a1 = new Aluno("Aula 1", 1);
+        Aluno a2 = new Aluno("Aula 2", 2);
+        Aluno a3 = new Aluno("Aula 3", 3);
+
+        cursos.matricula(a1);
+        cursos.matricula(a2);
+        cursos.matricula(a3);
+
+        cursos.getAlunos().forEach(aluno -> {
+            System.out.println(aluno);
+        });
+   }
+}

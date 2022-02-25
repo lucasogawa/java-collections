6b92642a526b1f224537956d3fa98d60bd016338
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-02-24 22:08:24 -0300

    Adding Verificando se está matriculado

---
+++ 07-equals-e-hashcode/03-verificando-se-esta-matriculando/Curso.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Curso {
+
+    private String nome;
+    private String instrutor;
+    private List<Aula> aulas = new LinkedList<Aula>();
+    private Set<Aluno> alunos = new HashSet<>();
+
+    public Curso(String nome, String instrutor) {
+        this.nome = nome;
+        this.instrutor = instrutor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getInstrutor() {
+        return instrutor;
+    }
+
+    public List<Aula> getAulas() {
+        return Collections.unmodifiableList(aulas);
+    }
+
+    public Set<Aluno> getAlunos() {
+        return Collections.unmodifiableSet(alunos);
+    }
+
+    public void adiciona(Aula aula) {
+        this.aulas.add(aula);
+    }
+
+    public int getTempoTotal() {
+        return this.aulas
+            .stream()
+            .mapToInt(Aula::getTempo)
+            .sum();
+    }
+
+    public void matricula(Aluno aluno){
+        this.alunos.add(aluno);
+    }
+
+    public boolean estaMatriculado(Aluno aluno) {
+        return this.alunos.contains(aluno);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Aluno outro = (Aluno) obj;
+
+        return this.nome.equals(outro.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return this.nome.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "[Curso: " + this.nome + ", tempo total: " + getTempoTotal() + ", aulas: [" + aulas + "]";
+    }
+}

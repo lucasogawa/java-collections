20cc7c955e47133ab970b36254613fd249fcff5c
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-03-03 22:19:55 -0300

    Adding TreeSet e Comparator

---
+++ 08-outros-sets-e-iterators/06-treeset-e-comparator/OrdenaPorIdade.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class OrdenaPorIdade implements Comparator<Funcionario> {
+    
+    @Override
+    public int compare(Funcionario funcionario, Funcionario outroFuncionario) {
+        return funcionario.getIdade() - outroFuncionario.getIdade();
+    }
+}

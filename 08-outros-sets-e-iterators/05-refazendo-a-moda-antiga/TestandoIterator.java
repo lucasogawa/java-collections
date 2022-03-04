f2e725b42f25c1f0eb00680608aeaf8fa5f9eb48
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-03-03 22:12:42 -0300

    Adding Refazendo a moda antiga

---
+++ 08-outros-sets-e-iterators/05-refazendo-a-moda-antiga/TestandoIterator.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class TestandoIterator {
+    public static void main(String[] args) {
+
+        List<String> letras = new LinkedList<>();
+        letras.add("A");
+        letras.add("B");
+        letras.add("C");
+
+        Iterator<String> iterador = letras.iterator();
+
+        while(iterador.hasNext()) {
+            System.out.println(iterador.next());
+        }
+    }
+}
